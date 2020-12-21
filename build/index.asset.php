c5e79f4a8c249e4256c66aa57a7368a60b58d472
artpi <artur.piszek@gmail.com>
2020-12-21 10:53:50 +0100

    built stuff

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dee52533c8158463d3f84b89c81cf4ff');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '80d9849c59c3201623bfd3fa1d47109d');
