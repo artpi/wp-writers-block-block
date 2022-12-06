e5ad4c2f0f32613f6a22af2bd70c31e7badbfc5b
artpi <artur.piszek@gmail.com>
2022-12-06 14:04:22 +0100

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2cb54db8b71d92236652');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'edeacae435cc5fdc74f9');
