9ee1f2255d322351b82f666a55023d77513bceab
artpi <artur.piszek@gmail.com>
2022-12-08 18:19:29 +0100

    more linting

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,4 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '51c10576115573a426ab');
+<?php return array(
+	'dependencies' => array( 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n' ),
+	'version'      => '51c10576115573a426ab',
+);
