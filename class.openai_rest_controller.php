a7e2f151ed94ebf096a7441b185d69cb92533f5a
artpi <artur.piszek@gmail.com>
2022-12-08 11:36:11 +0100

    format plus better logs

--- class.openai_rest_controller.php
+++ class.openai_rest_controller.php
@@ -2,12 +2,12 @@
 
 class OpenAI_REST_Controller extends WP_REST_Controller {
 
-    public $text_completion_cooldown_seconds = 10; // Allow new completion every X seconds. Will return cached result otherwise. 
-    public $image_generation_cache_timeout = 3600 * 24 * 31; // Cache images for a prompt for a month.
+	public $text_completion_cooldown_seconds = 10; // Allow new completion every X seconds. Will return cached result otherwise.
+	public $image_generation_cache_timeout   = 3600 * 24 * 31; // Cache images for a prompt for a month.
 
 	public function __construct() {
-		$this->namespace                       = 'wp/v2';
-		$this->rest_base                       = 'openai';
+		$this->namespace = 'wp/v2';
+		$this->rest_base = 'openai';
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
 	}
 
@@ -47,18 +47,19 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 		);
 	}
 
-    public function log_request_dalle_generation( array $response ) {
-        // Nothing here.
-    }
+	public function log_request_dalle_generation( $prompt, array $response ) {
+		// Nothing here.
+		error_log( 'Generating dalle images for prompt: ' . $prompt );
+	}
 
-    public function log_request_gpt_completion( array $response ) {
-        // Nothing for now.
+	public function log_request_gpt_completion( $prompt, array $response ) {
+		// Nothing for now.
 		error_log( 'OpenAI Completion: ' . json_encode( $response ) );
-    }
+	}
 
-    public function get_openai_token() {
+	public function get_openai_token() {
 		return get_option( 'openai-token' );
-    }
+	}
 
 	/**
 	 * Ensure the user has proper permissions
@@ -69,21 +70,21 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 		return current_user_can( 'edit_posts' );
 	}
 
-    protected function transient_name_for_prompt( $prompt ) {
-        return 'openai-image-' . md5( $prompt );
-    }
+	protected function transient_name_for_prompt( $prompt ) {
+		return 'openai-image-' . md5( $prompt );
+	}
 
 	public function request_dalle_generation( WP_REST_Request $request ) {
 		//We are saving responses as transients, so that we don't spam the API.
 		$parameters = $request->get_params();
 
-        $token = $this->get_openai_token();
+		$token = $this->get_openai_token();
 
 		if ( ! $token ) {
 			return new \WP_Error( 'token_missing', __( 'OpenAI Token is missing from configuration' ) );
 		}
 
-        $cache = get_transient( $this->transient_name_for_prompt( $parameters['prompt'] ) );
+		$cache = get_transient( $this->transient_name_for_prompt( $parameters['prompt'] ) );
 		if ( $cache ) {
 			return json_decode( $cache );
 		}
@@ -101,7 +102,7 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 						'n'               => 4, // Generate 4 options each time,
 						'size'            => '512x512',
 						'response_format' => 'b64_json', // This will return image body inside JSON, so that we don't have to deal with CORS on images.
-                        'user'            => strval( get_current_user_id() ) // This logs the user id on the OpenAI side so it's easier to detect abuse.
+						'user'            => strval( get_current_user_id() ), // This logs the user id on the OpenAI side so it's easier to detect abuse.
 					]
 				),
 				'method'      => 'POST',
@@ -109,7 +110,7 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 				'timeout'     => 60, // This may not be enough, but works so far.
 			)
 		);
-        $this->log_request_dalle_generation( $api_call );
+		$this->log_request_dalle_generation( $parameters['prompt'], $api_call );
 		if ( is_wp_error( $api_call ) ) {
 			return $api_call;
 		}
@@ -119,20 +120,20 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 		return $result;
 	}
 
-    protected function transient_name_for_completion() {
-        return 'openai-completion-' . get_current_user_id(); // Cache for each user, so that other users dont get weird cached version from somebody else.
-    }
+	protected function transient_name_for_completion() {
+		return 'openai-completion-' . get_current_user_id(); // Cache for each user, so that other users dont get weird cached version from somebody else.
+	}
 
 	public function request_gpt_completion( WP_REST_Request $request ) {
 		$parameters = $request->get_params();
 		$content    = strip_tags( $parameters['content'] );
-        $token = $this->get_openai_token();
+		$token      = $this->get_openai_token();
 
 		if ( ! $token ) {
 			return new \WP_Error( 'token_missing', __( 'OpenAI Token is missing from configuration' ) );
 		}
 
-        $cache = get_transient( $this->transient_name_for_completion() );
+		$cache = get_transient( $this->transient_name_for_completion() );
 		if ( $cache ) {
 			return array( 'prompts' => [ $cache ] );
 		}
@@ -149,7 +150,7 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 						'model'      => 'text-davinci-003', // Most capable model of GPT3
 						'prompt'     => $content,
 						'max_tokens' => 110, // This is length of generated prompt. A token is about 4 chars. I took 110 from Lex.page.
-                        'user'       => strval( get_current_user_id() ) // This logs the user id on the OpenAI side so it's easier to detect abuse.
+						'user'       => strval( get_current_user_id() ), // This logs the user id on the OpenAI side so it's easier to detect abuse.
 					]
 				),
 				'method'      => 'POST',
@@ -158,7 +159,7 @@ class OpenAI_REST_Controller extends WP_REST_Controller {
 			)
 		);
 
-        $this->log_request_gpt_completion( $api_call );
+		$this->log_request_gpt_completion( $content, $api_call );
 		if ( is_wp_error( $api_call ) ) {
 			return $api_call;
 		}
